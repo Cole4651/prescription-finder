69b5b9fdf35c449a0deed0e9977b1b85e9ce8229
nrthompson925 <nrthompson@bluevalleyk12.net>
2024-04-13 20:07:38 -0500

    add sign-up button to prevent softlock

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,11 +1,17 @@
 
-const FrontPage = function () {
+// const { getLoginPageUrl, getSignupPageUrl, getAccountPageUrl } = useHostedPageUrls();
+import { withAuthInfo, useRedirectFunctions, useLogoutFunction } from '@propelauth/react';
 
+
+const FrontPage = function () {
+    const { redirectToLoginPage, redirectToSignupPage, redirectToAccountPage } = useRedirectFunctions();
     return (
         <div className='container'>
             <div className='header'>
                 <div className='text'>Sign Up</div>
                 <div className='underline'></div>
+                <button onClick={() => redirectToSignupPage()}>Sign up</button>
+                
             </div>
         </div>
         )
