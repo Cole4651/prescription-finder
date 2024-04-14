e930c9a479859c41374aef859b77e5b48eee36b6
Cole Nola <garliccloves4651@gmail.com>
2024-04-13 19:29:39 -0500

    cole

--- src/components/mainpages/Homepage.js
+++ src/components/mainpages/Homepage.js
@@ -12,7 +12,7 @@ function Homepage () {
             </div>
             <div className="page">
                 <h2>Welcome to CalendarApp</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>"name" is a user-friendly prescription finder application designed to simplify the process of locating and obtaining prescribed medications by organizing scheduling. Whether you're a patient searching for a specific medication, a healthcare provider looking to streamline prescription management, or a caregiver assisting others with their healthcare needs, "name" is here to help make this process less annoying than it needs to be.</p>
             </div>
             <div className="fleximg">
                 <img className="promopic" src={randumb} alt="randumb" />
